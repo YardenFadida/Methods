0cdfbe1b7aaddb156a1eedd1324e6536032aa447
YardenFadida <101218103+YardenFadida@users.noreply.github.com>
2022-05-28 14:02:21 +0300

    Add files via upload

---
+++ compressed_methods.java
@@ -0,0 +1,40 @@
+/****************************************************************
+     * this method get array and compress him with zlib class       *
+     * @param data  the array that you want to compress             *
+     * @return output the compressed output array                   *
+     ***************************************************************/
+    public static byte[] compress(byte[] data) throws IOException {
+        Deflater deflater = new Deflater();
+        deflater.setInput(data);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
+        deflater.finish();
+        byte[] buffer = new byte[1024];
+        while (!deflater.finished()) {
+            int count = deflater.deflate(buffer); // returns the generated code... index
+            outputStream.write(buffer, 0, count);
+        }
+        outputStream.close();
+        byte[] output = outputStream.toByteArray();
+        return output;
+    }
+    
+    /******************************************************************
+     * this method get array and decompress him with zlib class       *
+     * @param data the array that you want to decompress              *
+     * @return output this output is the decompressed array           *
+     *****************************************************************/
+    public static byte[] decompress(byte[] data) throws IOException, DataFormatException {
+        Inflater inflater = new Inflater();
+        inflater.setInput(data);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
+        byte[] buffer = new byte[1024];
+        while (!inflater.finished()) {
+            int count = inflater.inflate(buffer);
+            outputStream.write(buffer, 0, count);
+        }
+        outputStream.close();
+        byte[] output = outputStream.toByteArray();
+        return output;
+    }
+
+
